902ce0c89626b584cf8f58ab018e3820789174c8
wandoy37 <riswandi035@gmail.com>
2023-03-29 22:39:31 +0800

    added message error login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,7 +40,11 @@
                 <div class="login-form">
                     <div class="form-group">
                         <label for="username" class="placeholder"><b>Username</b></label>
-                        <input id="username" name="username" type="text" class="form-control" required>
+                        <input id="username" name="username" type="text" class="form-control" required
+                            value="{{ old('username') }}">
+                        @error('username')
+                            <span class="text-danger"><b>Login gagal silahkan coba kembali</b></span>
+                        @enderror
                     </div>
                     <div class="form-group">
                         <label for="password" class="placeholder"><b>Password</b></label>
@@ -50,6 +54,9 @@
                                 <i class="icon-eye"></i>
                             </div>
                         </div>
+                        @error('password')
+                            <span class="text-danger"><b>Login gagal silahkan coba kembali</b></span>
+                        @enderror
                     </div>
                     <div class="form-group float-right">
                         <button type="submit" class="btn btn-secondary btn-round btn-lg">Sign In</button>
